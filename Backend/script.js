b3f6b46a669b57a473a44666df70e5c432d2d052
kasjaduddin <kasjaduddin@outlook.com>
2024-01-04 17:26:33 +0700

    feat: update attendance API

--- Backend/script.js
+++ Backend/script.js
@@ -1,15 +1,32 @@
 let guestName = document.getElementById("nama");
-let attendance = document.getElementById("kehadiran");
+let attendance = document.getElementById("kedatangan");
 let message = document.getElementById("ucapan");
 
-function chat(params) {
-    
-}
+// chat();
+// function chat(params) {
+//     fetch("https://e-wedding-invitation-d200d-default-rtdb.asia-southeast1.firebasedatabase.app/guestbook.json?auth=yQO7wliGgtnklppvbHcTCjZl2kT4Tsc69RhFxOt4")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         let baloon = "";
+//         let output = Object.entries(data);
+//         output.forEach((row) => {
+//             baloon += `
+//           <tr>
+//               <td>${row[1].nama}</td>
+//               <td>${row[1].nohp}</td>
+//               <td><button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</td>
+//               <td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Hapus</td>
+//           </tr>
+//           `;
+//         });
+//         tbody.innerHTML = tabel;
+//       });
+//   }
 
 function writeGuestBook(params) {
     let data = {
         nama: guestName.value,
-        kehadiran: attendance.value,
+        kedatangan: attendance.value,
         ucapan: message.value
     };
     fetch("https://e-wedding-invitation-d200d-default-rtdb.asia-southeast1.firebasedatabase.app/guestbook.json?auth=yQO7wliGgtnklppvbHcTCjZl2kT4Tsc69RhFxOt4", {
@@ -21,7 +38,7 @@ function writeGuestBook(params) {
     })
     .then((res) => res.json())
     .then((data) => {
-        chat();
+        // chat();
     });
     guestName.value = "";
     attendance.value = "";
