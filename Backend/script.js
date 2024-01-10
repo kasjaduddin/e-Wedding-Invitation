0ba516970ad8d6b77233e549af71330fea801aeb
kasjaduddin <kasjaduddin@outlook.com>
2024-01-10 18:59:56 +0700

    feat: update cover for desktop

--- Backend/script.js
+++ Backend/script.js
@@ -2,6 +2,21 @@ let guestName = document.getElementById("nama");
 let attendance = document.getElementById("kedatangan");
 let message = document.getElementById("ucapan");
 
+function guest(params) {
+    fetch("https://e-wedding-invitation-d200d-default-rtdb.asia-southeast1.firebasedatabase.app/guestbook.json?auth=yQO7wliGgtnklppvbHcTCjZl2kT4Tsc69RhFxOt4")
+      .then((res) => res.json())
+      .then((data) => {
+        let baloon = "";
+        let output = Object.entries(data);
+        output.forEach((row) => {
+            baloon += `
+            <p class="fs-53">${row[1].nama}</p>
+          `;
+        });
+        chatroom.innerHTML = baloon;
+      });
+}
+
 chat();
 function chat(params) {
     fetch("https://e-wedding-invitation-d200d-default-rtdb.asia-southeast1.firebasedatabase.app/guestbook.json?auth=yQO7wliGgtnklppvbHcTCjZl2kT4Tsc69RhFxOt4")
